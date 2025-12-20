cc6357835cd35b8035f026f0434c04195f1c46d9
atsdadsas <2418027.oa@sist.ac.jp>
2025-12-20 18:18:55 +0900

    googleAPIの設定

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
 
 # データベースの設定：library.db というファイルに保存する
 # app.configの[SQLALCHEMY_DATABASE_URI]=はsqliteの--というファイルです。と設定している。内の値を
 # sqliteはSQライトで、:///はここから先がファイル名という目印。
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql://library_db_w72s_user:HJc0olzxHzIcvkF7Xpb48A7ueppCM16W@dpg-d535d8v5r7bs73dj5qgg-a/library_db_w72s', 'sqlite:///library.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -30,6 +31,25 @@ def index():
     # 全ての本をデータベースから取得して画面に表示
     books = Book.query.all()
     return render_template('index.html', books=books)
+
+@app.route("/add_by_isbn",methods=["POST"])
+def add_by_isbn():
+    isbn=request.form.get("isbn")
+    url=f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}"
+    response = requests.get(url)
+    data = response.json()
+    
+    if "items" in data:
+        book_info =data["items"][0]["volumeInfo"]
+        title = book_info.get("title","不明なタイトル")
+        author=",".join(book_info.get("authors",["不明な著者"]))
+        new_book=Book(title=title,author=author,status="蔵書")
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect("/")
+    else:
+        return "本が見つかりませんでした",404
+    
 
 @app.route('/add', methods=['POST'])
 def add_book():
