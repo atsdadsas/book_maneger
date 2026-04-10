692f9f87d1440673a03885304d63416391e1e73a
atsdadsas <atat00140514@icloud.com>
2026-04-10 14:35:50 +0900

    test

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -215,4 +215,4 @@ def delete_book(book_id):
 if __name__ == '__main__':
     # サーバーで動かすときは debug=False にするのが鉄則です
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
